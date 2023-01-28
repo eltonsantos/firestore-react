394fcd26e1d05c0d3334abe0a24f983120ada6ee
Elton Santos <eltonaxl@hotmail.com>
2023-01-28 02:24:49 -0300

    Remove code useless

--- service/firebase-config.ts
+++ service/firebase-config.ts
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore'
-import {  } from 'firebase/firestore'
+import { getFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -13,7 +12,7 @@ const firebaseConfig = {
   projectId: "firestore-react-7655d",
   storageBucket: "firestore-react-7655d.appspot.com",
   messagingSenderId: "459270503378",
-  appId: "1:459270503378:web:61761b82bd722af1d2260b"
+  appId: "1:459270503378:web:61761b82bd722af1d2260b",
 };
 
 // Initialize Firebase
